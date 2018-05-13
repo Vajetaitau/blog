6d49d7e0691753a67fa0e3bda7f4f040fc152f4a
Karolis Tarasaitis <kta@siteimprove.com>
2018-05-13 17:41:29 +0200

    #004: Added vue-frontend project. Configured multiple elements

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Navigation />
+
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation.vue'
+
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js Appxxxxxxxx"
+    };
+  },
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-frontend/src/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <a :key="index" v-for="(menuItem, index) in menuItems" href="xasd">
+      {{ menuItem.name }}
+    </a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navigation",
+  data() {
+    return {
+      menuItems: [
+        { name: "First link" },
+        { name: "Secaaaa link" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
+
+
